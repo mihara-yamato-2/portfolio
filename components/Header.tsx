e051256763e4b38391f541a03b2613188f4226a5
mihara-yamato-2 <y.mihara@philduct.com>
2025-07-04 00:47:45 +0900

    change yaml

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { profile } from "../data/profile";
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
