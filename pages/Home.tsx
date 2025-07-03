674c36a745605f266671569c79e7aa45340605cd
mihara-yamato-2 <y.mihara@philduct.com>
2025-07-04 02:50:41 +0900

    improve site

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { profile } from "../data/profile";
+// import { profile } from "../data/profile";
 import { blogPosts } from "../data/blog";
-import { GitHubIcon } from "../components/icons/GitHubIcon";
+// import { GitHubIcon } from "../components/icons/GitHubIcon";
 import HeroSection from "../components/HeroSection";
 
 import ProjectsSection from "../components/ProjectsSection";
