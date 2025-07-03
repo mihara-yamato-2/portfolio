8ae5fe599376043455082f827c96f051df9b7afa
mihara-yamato-2 <y.mihara@philduct.com>
2025-07-04 02:40:07 +0900

    improve site

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // For GitHub Pages deployment at yamato-mihara.github.io/portfolio
+  base: "/portfolio/", // For GitHub Pages deployment at yamato-mihara.github.io/portfolio
 });
