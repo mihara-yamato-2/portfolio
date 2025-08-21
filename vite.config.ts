3f5d41a2facd0ce0e8c64f4e228bd484d3636383
mihara-yamato-2 <mihayama1005@gmail.com>
2025-08-21 17:28:24 +0900

    Fix GitHub Pages URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // For GitHub Pages deployment at yamato-mihara.github.io/portfolio
+  base: "/portfolio/", // For GitHub Pages deployment at mihara-yamato-2.github.io/portfolio
 });
